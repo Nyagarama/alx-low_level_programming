dcc09a3632180dd7157be29340e8ef4674b77126
Nyagarama <mochristoper27@gmail.com>
2023-03-08 16:55:41 -0800

    Header file

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * File name: main.h
+ * Description: Header file containing prototypes for all functions
+ */
+
+int _putchar(char);
+void _puts_recursion(char *s);
+
+#endif
