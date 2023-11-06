a66e798d3a96b674e6909e92ed78555f0bfd2ffd
Nyagarama <mochristopher27@gmail.com>
2023-11-06 13:21:10 -0800

    Dog Details

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * struct dog - Dog details
+ * @name: Dog's name
+ * @age: Dog's age
+ * @owner: Dog owner
+ * @Description: contains dog's identification details
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
